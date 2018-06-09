3fc904cbd3ab416bfb8a1e0b0e79c0492a0bda76
niezhiliang <1132263176@qq.com>
2018-06-09 11:05:05 +0800

    m

--- rm-images.sh
+++ rm-images.sh
@@ -1,5 +1,11 @@
 
+docker rmi niezhiliang/spring-cloud-eureka:v1.0.0
 
 docker rmi niezhiliang/spring-cloud-gateway:v1.0.0
 
 docker rmi niezhiliang/spring-cloud-config:v1.0.0
+
+docker rmi niezhiliang/service-a:v1.0.0
+
+docker rmi niezhiliang/service-b:v1.0.0
+
